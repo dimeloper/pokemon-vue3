7c0cec0eb4bc1c6a417810e3754c7832a36d4d90
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2023-01-02 20:08:18 +0000

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-14 ">
+    <span class="flex justify-center text-4xl text-yellow-700">
+     Pokémon Picker&nbsp;
+      <img src="src/assets/pokeball.png" class="w-10 h-10"/>
+    </span>
+    <router-view />
+  </div>
+</template>
